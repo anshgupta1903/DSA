b69c5737d71d0bfd7cfa1dad968d39fb718c58a8
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-09-05 23:21:49 +0530

    Time: 3 ms (91.02%) | Memory: 44.3 MB (40.71%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -4,10 +4,13 @@ class Solution {
         st.push(nums2[nums2.length-1]);
         int[] arr = new int[nums2.length];
         arr[nums2.length-1] = -1;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(nums2[nums2.length-1], -1);
         for(int i=nums2.length-2;i>=0;i--){
             if(st.peek()>nums2[i]){ 
                 
-                arr[i] = st.peek();
+                // arr[i] = st.peek();
+                map.put(nums2[i],st.peek());
                 st.push(nums2[i]);
             }
             else{
@@ -19,19 +22,24 @@ class Solution {
                                     
 
                 if(st.isEmpty()) {
-                    arr[i] = -1;
+                    // arr[i] = -1;
+                    map.put(nums2[i], -1);
                     }
                 else{
-                    arr[i] = st.peek();
+                    // arr[i] = st.peek();
+                    map.put(nums2[i], st.peek());
                 }
                 st.push(nums2[i]);
             }
         }
         int[] res = new int[nums1.length];
+        // for(int i=0;i<nums1.length;i++){
+        //     for(int j=0;j<nums2.length;j++){
+        //         if(nums1[i]==nums2[j]) res[i] = arr[j];
+        //     }
+        // }
         for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]) res[i] = arr[j];
-            }
+            res[i] = map.get(nums1[i]);
         }
         return res;
         
