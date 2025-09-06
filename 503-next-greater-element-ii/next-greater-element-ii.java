201ec42132283d53741c9973df74cc225ee2f6d5
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-09-06 11:05:56 +0530

    Time: 17 ms (44.22%) | Memory: 46.2 MB (73.96%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -2,39 +2,25 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int[] arr = new int[nums.length];
         Stack<Integer> st = new Stack<>();
-        for(int i=nums.length-1;i>=0;i--){
-            if(!st.isEmpty() && st.peek()<=nums[i]){
-                while(!st.isEmpty() && !(nums[i]<st.peek())){
+        for(int i=2*nums.length-1;i>=0;i--){
+            int n=nums.length;
+            int idx=i%n;
+            
+                while(!st.isEmpty() && nums[st.peek()]<=nums[idx]){
                     st.pop();
                 }
-            }
+            
             if(st.isEmpty()){
-                arr[i]=Integer.MIN_VALUE;
-                st.push(nums[i]);
+                arr[idx]=-1;
+                
             }
-            else if(st.peek()>nums[i]){
-                arr[i] = st.peek();
-                st.push(nums[i]);
+            else{
+                arr[idx] = nums[st.peek()];
+                
             }
+            st.push(idx);
         }
 
-        st.clear();
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==Integer.MIN_VALUE){
-                for(int j=0;j<i;j++){
-                    if(nums[j]>nums[i]){
-                        arr[i] = nums[j];
-                        break;
-                    }
-                    
-                }
-            }
-        }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==Integer.MIN_VALUE){
-                arr[i] = -1;
-            }
-        }
         return arr;
     }
 }
