0466335ea0d8625c1b2ce9e45a7a5a72fb4ded90
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-10-12 00:01:34 +0530

    Time: 2 ms (21.74%) | Memory: 42.6 MB (47.75%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,70 +1,44 @@
 class Solution {
+    public long help(String s, int idx, int end){
+        if(idx == end){
+            return 0;
+        }
+        long num = help(s, idx+1, end);
+        long x = s.charAt(idx) - '0';
+        long power = (int)Math.pow(10, end - idx - 1);
+        return x * power + num;
+    }
+
     public int myAtoi(String s) {
-        StringBuilder sb = new StringBuilder(s.trim());
-        int i=0;
-        boolean isneg = false;
-        if(sb.length() == 0) return 0;
+        s = s.trim();
+        if(s.length() == 0) return 0;
 
-        // System.out.println(sb.charAt(0)-'0');
-        if(sb.charAt(0)=='+') sb.delete(0,1);
-        else if(sb.charAt(0)=='-') {
-            sb.delete(0,1);
-            isneg = true;
-        }
+        int startIdx = 0;
+        boolean isNegative = false;
 
-        else if(sb.charAt(0)-'0'>9 || sb.charAt(0)-'0'<0){
-            return 0;}
-
-        
-        while(i<sb.length()){
-            // if(i==sb.length()) break;
-            char c = sb.charAt(i);
-            if(c==' '){
-                sb.delete(i,sb.length());
-            }
-            else if(c=='+'){
-                sb.delete(i, sb.length());
-                break;
-            }
-            else if(c=='-'){
-                sb.delete(i, sb.length());
-                break;
-            }
-            else if(Character.isDigit(c)){
-                i++;
-                continue;
-            }
-            else{
-                sb.delete(i, sb.length());
-            }
-            i++;
-        }
-        if(sb.length()==0) return 0;
-        long res = 0;
-        System.out.println(sb);
-        if(sb.charAt(0)=='-'){
-            sb.delete(0,1);
-            isneg = true;
-        }
-        
-        for(char c:sb.toString().toCharArray()){
-        int digit = c - '0';  // define digit here
-
-        if(!isneg) {
-        if(res > (Integer.MAX_VALUE - digit) / 10) {
-            return Integer.MAX_VALUE;
-        }
-        } else {
-            if(-res < (Integer.MIN_VALUE + digit) / 10) {
-            return Integer.MIN_VALUE;
-            }
-        }
-            res = (res*10) + (c-'0');
+        if(s.charAt(0) == '-'){
+            isNegative = true;
+            startIdx = 1;
+        } else if(s.charAt(0) == '+'){
+            startIdx = 1;
         }
 
-        if(isneg == true) res*=(-1);
-        // if(res < Integer.MIN_VALUE) res = Integer.MIN_VALUE;
-        // if(res > Integer.MAX_VALUE) res = Integer.MAX_VALUE;
-         return (int)res;
+        int endIdx = startIdx;
+        while(endIdx < s.length() && Character.isDigit(s.charAt(endIdx))){
+            endIdx++;
+        }
+
+        long result = help(s, startIdx, endIdx);
+        if (isNegative) result = -result;
+
+        if (result > Integer.MAX_VALUE) {
+        result = Integer.MAX_VALUE;
+        }  
+        if(result < Integer.MIN_VALUE){
+            result = Integer.MIN_VALUE;
+        }
+
+    return (int)result;
+
     }
 }
