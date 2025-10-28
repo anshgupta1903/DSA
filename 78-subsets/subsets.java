8c13a77a2cbd4cf64e5062ad17f1988f1eef5f32
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-10-28 12:24:37 +0530

    Time: 1 ms (47.98%) | Memory: 42.7 MB (67.06%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,29 +1,22 @@
-import java.util.*;
-
 class Solution {
-    public List<List<Integer>> allsub(int[] arr, List<Integer> res, int i) {
-        List<List<Integer>> fi = new ArrayList<>();
-
-        // Base case: add current subset and return
-        if (i == arr.length) {
-            fi.add(new ArrayList<>(res)); 
-            return fi;
+    public void help(int[] nums, int idx, List<List<Integer>> res, List<Integer> list){
+        if(idx == nums.length){
+            // res.add(new ArrayList<>(list));
+            return;
         }
-
-        // Exclude current element
-        fi.addAll(allsub(arr, res, i + 1));
-
-        // Include current element
-        res.add(arr[i]);
-        fi.addAll(allsub(arr, res, i + 1));
-
-        // Backtrack
-        res.remove(res.size() - 1);
-
-        return fi;
+        for(int i=0;i<nums.length;i++){
+            if(i==idx) {
+                list.add(nums[i]);
+                res.add(new ArrayList<>(list));
+                help(nums, ++idx, res, list);
+                list.remove(list.size()-1);
+            }
+        }
     }
-
     public List<List<Integer>> subsets(int[] nums) {
-        return allsub(nums, new ArrayList<>(), 0);
+        List<List<Integer>> res = new ArrayList<>();
+        help(nums, 0, res, new ArrayList<>());
+        res.add(new ArrayList<>());
+        return res;
     }
 }
