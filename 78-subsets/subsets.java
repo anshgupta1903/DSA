0e3dff32ab96cd47840ba94dfa5c382b87dcca6b
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-11-04 10:05:41 +0530

    Time: 1 ms (49.79%) | Memory: 44.3 MB (5.19%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,21 +1,26 @@
 class Solution {
-    public void help(int[] nums, int idx, List<List<Integer>> res, List<Integer> list){
-        if(idx == nums.length){
-            // res.add(new ArrayList<>(list));
+
+    public void backtrack(int[] nums, int i, List<List<Integer>> res, List<Integer> help){
+
+        if(i==nums.length) {
+            // res.add(new ArrayList<>(help));
             return;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(i==idx) {
-                list.add(nums[i]);
-                res.add(new ArrayList<>(list));
-                help(nums, ++idx, res, list);
-                list.remove(list.size()-1);
+            }
+        
+        for(int j=0;j<nums.length;j++){
+            if(j==i){
+            help.add(nums[j]);
+            res.add(new ArrayList<>(help));
+            backtrack(nums, ++i, res, help);
+            help.remove(help.size()-1);
             }
         }
     }
+
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        help(nums, 0, res, new ArrayList<>());
+        List<Integer> help = new ArrayList<>();
+        backtrack(nums, 0, res, help);
         res.add(new ArrayList<>());
         return res;
     }
