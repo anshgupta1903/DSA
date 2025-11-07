005056b5553052e94fc45942b2f557925ed66316
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-11-07 10:45:55 +0530

    Time: 4 ms (37.25%) | Memory: 45.4 MB (6.16%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,47 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // int[] arr = new int[nums2.length];
         Stack<Integer> st = new Stack<>();
-        st.push(nums2[nums2.length-1]);
-        int[] arr = new int[nums2.length];
-        arr[nums2.length-1] = -1;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(nums2[nums2.length-1], -1);
-        for(int i=nums2.length-2;i>=0;i--){
-            if(st.peek()>nums2[i]){ 
-                
-                // arr[i] = st.peek();
-                map.put(nums2[i],st.peek());
-                st.push(nums2[i]);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        st.push(nums2[0]);
+        for(int i=1;i<nums2.length;i++){
+            while(!st.isEmpty() && (st.peek() < nums2[i])){
+                map.put(st.pop(), nums2[i]);
             }
-            else{
-                while(!st.isEmpty() && st.peek() <= nums2[i]){
-                    
-                    st.pop();
-                    
-                    }
-                                    
-
-                if(st.isEmpty()) {
-                    // arr[i] = -1;
-                    map.put(nums2[i], -1);
-                    }
-                else{
-                    // arr[i] = st.peek();
-                    map.put(nums2[i], st.peek());
-                }
-                st.push(nums2[i]);
-            }
+            if(st.isEmpty()) map.put(nums2[i], -1);
+            // else map.put(nums2[i], st.peek());
+            st.push(nums2[i]);
         }
-        int[] res = new int[nums1.length];
-        // for(int i=0;i<nums1.length;i++){
-        //     for(int j=0;j<nums2.length;j++){
-        //         if(nums1[i]==nums2[j]) res[i] = arr[j];
-        //     }
-        // }
+    
         for(int i=0;i<nums1.length;i++){
-            res[i] = map.get(nums1[i]);
+            nums1[i] = map.getOrDefault(nums1[i], -1);
         }
-        return res;
-        
+        return nums1;
     }
 }
