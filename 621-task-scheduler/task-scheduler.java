8b42d0f232ac09da14446afd27fe8ae214ec2e5d
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-11-11 17:46:07 +0530

    Time: 2 ms (98.95%) | Memory: 48.2 MB (9.54%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -6,17 +6,12 @@ class Solution {
             max = Math.max(max, freq[i]);
         }
         int count = 0;
-        // int unique = 0;
         for(int i=0;i<freq.length;i++) {
-            // System.out.println(freq[i]);
             if(max == freq[i]) count++;
-            // if(freq[i] > 0) unique = 0;
         }
-        // System.out.println(count);
-        // if(tasks.length > n*max) return tasks.length;
-        // else {
-            return Math.max(tasks.length,(max - 1) * (n + 1) + count ) ;
-        // }
+        
+        return Math.max(tasks.length,max + (max-1)*n + count-1);
+        
     }
     private int[] freqEle(char[] tasks) {
         int[] freq = new int[26];
