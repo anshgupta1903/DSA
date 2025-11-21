2e632fe3432cd7e3a368b4e17398d843373f750a
Ansh Gupta <145745599+anshgupta1903@users.noreply.github.com>
2025-11-21 22:17:43 +0530

    Time: 7 ms (67.20%) | Memory: 46.2 MB (11.08%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -6,17 +6,22 @@ class Solution {
             sum+=i;
         }
         // total = sum;
+        int n = nums.length;
+        dp = new int[n+1][sum+1];
+        for(int[] a : dp){
+            Arrays.fill(a, -1);
+        }
         return help(nums, target, nums.length-1, sum,0);
     }
-
+    int[][] dp;
     public int help(int[] nums, int target, int n, int sum1, int sum2){
         // if(sum1-sum2 == target) return 1;
         if(n<0) return sum1-sum2==target ? 1:0;
-        
+        if(dp[n][sum1]!=-1) return dp[n][sum1];
         // if(sum1-nums[n])
         int take = help(nums, target, n-1, sum1-nums[n], sum2+nums[n]);
         int notTake = help(nums, target, n-1, sum1, sum2);
-        return take+notTake;
+        return dp[n][sum1] = take+notTake;
 
     }
 }
